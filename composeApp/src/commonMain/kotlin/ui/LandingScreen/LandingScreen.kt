3e20762517cb51f3e4f4e18665a7f4eb8aa7d368
hamza <hamza.919p@gmail.com>
2024-05-24 18:52:09 +0200

    Landing screen initial commit

--- composeApp/src/commonMain/kotlin/ui/LandingScreen/LandingScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/LandingScreen/LandingScreen.kt
@@ -1,8 +1,14 @@
-
-import androidx.compose.material.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 
 @Composable
 fun LandingScreen() {
+
+    MaterialTheme {
+        Scaffold {
+
+        }
+    }
 
 }
