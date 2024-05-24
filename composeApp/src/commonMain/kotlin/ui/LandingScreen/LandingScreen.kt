a66101c6d13f97887c6c3b3d19c7d769595d9337
hamza <hamza.919p@gmail.com>
2024-05-24 18:53:46 +0200

    top app bar inital commit

--- composeApp/src/commonMain/kotlin/ui/LandingScreen/LandingScreen.kt
+++ composeApp/src/commonMain/kotlin/ui/LandingScreen/LandingScreen.kt
@@ -1,14 +1,23 @@
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 
 @Composable
 fun LandingScreen() {
 
     MaterialTheme {
-        Scaffold {
+        Scaffold(topBar = { TopAppBar() }) {
 
         }
     }
 
+}
+
+@Composable
+fun TopAppBar() {
+    Column {
+        Text("Landing screen")
+    }
 }
